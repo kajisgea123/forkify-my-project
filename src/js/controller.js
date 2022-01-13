b00b7a21218c71c7320ec4c024d34ee470435d86
geakajis <geakajis@gmail.com>
2022-01-13 13:59:28 +0100

    Added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -117,6 +117,9 @@ const controlAddRecipe = async function (newRecipe) {
     addRecipeView.renderError(error.message);
   }
 };
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
 
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
@@ -126,6 +129,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
